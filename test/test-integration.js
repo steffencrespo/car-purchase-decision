94067de60fd650afb3f2a434ed81b1e5b2b75dee
Leonardo Steffen <leosteffen84@gmail.com>
2017-09-18 00:40:22 -0400

    adds basic test

---
+++ test/test-integration.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const {app} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Static Contents', function() {
+
+	it('should return 200 on hitting the root url', function(){
+		return chai.request(app)
+			.get('/')
+			.then(function(res) {
+				res.should.have.status(200);
+			});
+	});
+
+});
