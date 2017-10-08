7e840ea3aa39b286ff64e37ba36f7c3c6443e280
Leonardo Steffen <leosteffen84@gmail.com>
2017-10-08 17:47:01 -0400

    fixes typo that was making authentication fail

--- auth/router.js
+++ auth/router.js
@@ -6,7 +6,7 @@ const config = require('../config');
 
 const createAuthToken = user => {
   return jwt.sign({user}, config.JWT_SECRET, {
-    subject: user.name,
+    subject: user.username,
     expiresIn: config.JWT_EXPIRY,
     algorithm: 'HS256'
   });
