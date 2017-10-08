9aac9244969b932a5383c49ec85312e48fe10eb9
Leonardo Steffen <leosteffen84@gmail.com>
2017-10-08 15:06:00 -0400

    adds authentication to purchaseList endpoint

--- auth/index.js
+++ auth/index.js
@@ -1,4 +1,4 @@
-const router = require('./router');
+const {router} = require('./router');
 const {basicStrategy, jwtStrategy} = require('./strategies');
 
 module.exports = {router, basicStrategy, jwtStrategy};
--- server.js
+++ server.js
@@ -10,18 +10,29 @@ const {router: authRouter, basicStrategy, jwtStrategy} = require('./auth');
 
 mongoose.Promise = global.Promise;
 
-const app = express();
-app.use(bodyParser.json());
-app.use(morgan('common'));
-app.use(express.static('public'));
-
 const {PORT, DATABASE_URL} = require('./config');
 const {CarsList, Car} = require('./models');
+
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    if (req.method === 'OPTIONS') {
+        return res.send(204);
+    }
+    next();
+});
 
 // Authentication specifics
 app.use(passport.initialize());
 passport.use(basicStrategy);
 passport.use(jwtStrategy);
+
+app.use(bodyParser.json());
+app.use(morgan('common'));
+app.use(express.static('public'));
 
 app.use('/api/users/', usersRouter);
 app.use('/api/auth/', authRouter);
@@ -65,7 +76,7 @@ app.post('/purchaseList', (req, res) => {
   });
 });
 
-app.get('/purchaseList', (req, res) => {
+app.get('/purchaseList', passport.authenticate('jwt', {session: false}), (req, res) => {
   Car
     .find()
     .limit(10)
