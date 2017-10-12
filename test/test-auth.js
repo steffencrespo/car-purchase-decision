9ed271fd41213e9b97f6fdf394936ab74224fc03
Leonardo Steffen <leosteffen84@gmail.com>
2017-10-12 11:13:45 -0400

    adds tests for auth endpoint

--- test/test-auth.js
+++ test/test-auth.js
@@ -9,9 +9,6 @@ const {JWT_SECRET} = require('../config');
 
 const expect = chai.expect;
 
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
 describe('Auth endpoints', function() {
