5da7e48007ea6f36d8d3121121681c79250b6932
Leonardo Steffen <leosteffen84@gmail.com>
2017-10-13 22:35:30 -0400

    removes carslistschema and references to ir

--- models.js
+++ models.js
@@ -32,33 +32,6 @@ carSchema.statics.findByUserId = function(id) {
 return this.find( {userId: id} );
 };
 
-const carsListSchema = mongoose.Schema({
-  listName: {type: String, required: true},
-  cars: [ 
-    {
-      id: String,
-      make: String,
-      model: String,
-      year: String,
-      trim: String,
-      engine: String,
-      dealerUrl: String,
-      listedPrice: Number,
-      sellerName: String,
-      comments: String
-    }
-  ]
-});
-
-carsListSchema.methods.apiRepr = function() {
-  return {
-    id: this._id,
-    listName: this.listName,
-    cars: this.cars
-  }
-}
-
 const Car = mongoose.model('Car', carSchema)
-const CarsList = mongoose.model('CarsList', carsListSchema);
 
-module.exports = {CarsList, Car};
+module.exports = {Car};
