3b83f6be88dc249a7010e6f63b10c37d8ba722d3
Leonardo Steffen <leosteffen84@gmail.com>
2017-10-21 23:38:54 -0400

    removes deactivated tests

--- test/test-integration.js
+++ test/test-integration.js
@@ -1,4 +1,4 @@
-/*
+
 const faker = require('faker');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -17,46 +17,11 @@ let testUserIds = [];
 
 chai.use(chaiHttp);
 
-function tearDownDb() {
-	console.warn('Deleting database');
-	return mongoose.connection.dropDatabase();
-}
 
-function importDatabase() {
-	let exec = require('child_process').exec
-	let usersCommand = 'mongoimport --db test-cars-list-app -c users --drop --file ./users-dataset.json'
-	exec(usersCommand, (err, stdout, stderr) => {
-	  // check for errors or if it was succesfuly
-	  console.log('=========adding users');
-	});
-
-	let carsCommand = 'mongoimport --db test-cars-list-app -c cars --drop --file ./cars-no-list-dataset.json'
-	exec(carsCommand, (err, stdout, stderr) => {
-	  // check for errors or if it was succesfuly
-	  console.log('=========adding cars');
-	});
-}
-
-function runServerAndPopulateDB() {
-	// runServer(TEST_DATABASE_URL);
-	importDatabase();
-}
 
 describe('Cars App', function() {
 
-	before(function() {
-    return runServerAndPopulateDB();
-  });
-
-  // after(function() {
-  //   return tearDownDb();
-  // });
-
-  after(function() {
-    return closeServer;
-  });
-
-	xdescribe('Root', function() {
+	describe('Root', function() {
 
 		it('should return 200 on hitting the root url', function(){
 			return chai.request(app)
@@ -69,72 +34,7 @@ describe('Cars App', function() {
 
 	});
 
-	describe('User Authentication', function() {
-		it('Should reject requests with incorrect passwords', function() {
-		    return chai
-		        .request(app)
-		        .post('/api/auth/login')
-		        .auth('test1', 'wrongPassword')
-		        .then(() =>
-		            expect.fail(null, null, 'Request should not succeed')
-		        )
-		        .catch(err => {
-		            if (err instanceof chai.AssertionError) {
-		                throw err;
-		            }
-
-		            const res = err.response;
-		            expect(res).to.have.status(401);
-		        });
-		});
-	});
-
-	xdescribe('User Registration', function(){
-		it('should return 200 and the signup page on hitting /signup', function(){
-			return chai.request(app)
-			.get('/signup')
-			.then(function(res) {
-				res.should.have.status(200);
-			});
-		});
-
-		it('should return 200 and the signup page on hitting /signup', function(){
-			return chai.request(app)
-			.post('/signup')
-			.then(function(res) {
-				res.should.have.status(201);
-			});
-		});
-	});
-
-	xdescribe('Car Details Calls', function() {
-		it('should return 200 and the car details page on a GET request to carDetails', function() {
-			return chai.request(app)
-			.get('/carDetails')
-			.then(function(res) {
-				res.should.have.status(200);
-			});
-		});
-	});
-
 	describe('Purchase List Calls', function(){
-
-		xit('should return 200 and html file on a GET request to purchaseList method', function(){
-			// return chai.request(app)
-			// get('/purchaseList')
-			// then(function(res) {
-			// 	res.should.have.status(200);
-			// 	res.should.have.header('content-type', 'text/html; charset=UTF-8');
-			// });
-			let res;
-			return chai.request(app)
-				.get('/purchaseList')
-				.then(function(_res) {
-					res = _res;
-					res.should.have.status(200);
-				})
-
-		});
 
 		it('should return 200 and html file on a POST request to purchaseList method', function(){
 			return chai.request(app)
@@ -163,4 +63,3 @@ describe('Cars App', function() {
 		});
 	});
 });
-*/
